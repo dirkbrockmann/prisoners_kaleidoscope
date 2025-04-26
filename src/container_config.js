58fb451a4a3f68c483b11d1deadc61f2e570ac47
Dirk Brockmannn <brockmann.dirk@gmail.com>
2025-04-26 12:24:15 +0200

    prep release

--- src/container_config.js
+++ src/container_config.js
@@ -9,6 +9,6 @@ export default {
 	controls_size: {width:480,height:480},
 	display_class:" tw:p-0",
 	controls_class:"tw:p-0",
-	container_class:"tw:font-sans tw:grid tw:grid-cols-1 tw:sm:grid-cols-2 tw:sm:gap-4 tw:px-1 tw:sm:p-0 tw:m-8"
+	container_class:"tw:font-sans tw:font-light tw:grid tw:grid-cols-1 tw:sm:grid-cols-2 tw:sm:gap-4 tw:px-1 tw:sm:p-0 tw:m-8"
 }
 
